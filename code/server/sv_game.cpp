5f0e72b08fa6efe443c75bc44d71f79a299b77d7
redsaurus <redsaur@redsaurus.net>
2015-12-14 21:06:25 +0000

    renaming outputs

--- code/server/sv_game.cpp
+++ code/server/sv_game.cpp
@@ -1046,7 +1046,7 @@ void SV_InitGameProgs (void) {
 #ifdef JK2_MODE
 	const char *gamename = "jospgame";
 #else
-	const char *gamename = "jacustomgame";
+	const char *gamename = "jaenhancedgame";
 #endif
 
 	GetGameAPIProc *GetGameAPI;
